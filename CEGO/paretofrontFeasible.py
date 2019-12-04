7798214c6fd9a61290e401f69a25546d726d05ec
Roy de Winter <roy-win@hotmail.com>
2019-12-04 11:57:05 +0100

    Update paretofrontFeasible.py

--- CEGO/paretofrontFeasible.py
+++ CEGO/paretofrontFeasible.py
@@ -4,32 +4,39 @@ Created on Mon Oct  9 10:36:45 2017
 
 @author: r.dewinter
 """
-
 import numpy as np
 
-def paretofrontFeasible(objectives, constraints):
+def paretofrontFeasible(costs, constraints):
     """
-    :param objectives: An (n_points, n_objectives) array
-           constraints: An (n_points, n_constraints) array
-    :return: A (n_points, ) boolean array, indicating whether each point is Pareto efficient,
-             pareto efficient points are only the feasible ones
-             Feasible: constraints <= 0 for all constraint values. 
+    Find the pareto-efficient points
+    :param costs: An (n_points, n_costs) array
+    :param return_mask: True to return a mask
+    :return: An array of indices of pareto-efficient points.
+        If return_mask is True, this will be an (n_points, ) boolean array
+        Otherwise it will be a (n_efficient_points, ) integer array of indices.
     """
+    
     n_constraints = constraints.shape[1]
-    feasible = np.sum(constraints <= 0, axis = 1) == n_constraints
+    feasible = sum(constraints <= 0, axis = 1) == n_constraints
     
-    S = {} #pareto dominant points
-    def update(p):
-        if any(all(S[q] <= p) for q in S):
-           return
-        for q in [q for q in S if all(p<=S[q])]:
-            del S[q]
-        S[frozenset(p)] = p
+    indexes = np.arange(len(feasible))
+    findexes = indexes[feasible]
+    n_points1 = costs.shape[0]
     
-    for obj in objectives[feasible]:
-        update(obj) #add every point to dominant points if it is dominant
+    costs = costs[findexes]
     
-    S = np.array(list(S.values())) #make list from dominant points
+    is_efficient = np.arange(costs.shape[0])
+    next_point_index = 0  # Next index in the is_efficient array to search for
+    while next_point_index<len(costs):
+        nondominated_point_mask = np.any(costs<=costs[next_point_index], axis=1)
+        nondominated_point_mask[next_point_index] = True
+        is_efficient = is_efficient[nondominated_point_mask]  # Remove dominated points
+        costs = costs[nondominated_point_mask]
+        next_point_index = np.sum(nondominated_point_mask[:next_point_index])+1
+
+    pff = findexes[is_efficient]
     
-    a = (np.sum(np.isin(objectives,S),axis=1)+feasible)==(objectives.shape[1]+1) #make array of booleans to indicate which are dominant
-    return(a)
+    is_efficient_mask = np.zeros(n_points1, dtype = bool)
+    is_efficient_mask[pff] = True
+    
+    return is_efficient_mask
